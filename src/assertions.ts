ee2360c7a672d0ee8435abe5d9d45ad5ede40aa2
Kylee Tilley <kyleetilley@gmail.com>
2020-05-08 23:14:58 -0500

    Implement toThrow assertion

--- src/assertions.ts
+++ src/assertions.ts
@@ -29,3 +29,68 @@ export const not = <T>(assertionFn: AssertionFn<T>) => (value: any) => {
   const assertion = assertionFn(value);
   return { ...assertion, not: true, pass: !assertion.pass };
 };
+
+export const toThrow = <T>(errorMatching?: string | Error) => (value: any) => {
+  try {
+    value.call(null);
+    return {
+      value,
+      pass: false,
+      message: `to throw`,
+    };
+  } catch (e) {
+    if (errorMatching) {
+      if (typeof errorMatching === "string") {
+        return errorMatchingString(e, errorMatching);
+      } else {
+        return errorMatchingClass(e, errorMatching);
+      }
+    } else {
+      return {
+        value,
+        pass: true,
+        message: `to throw`,
+      };
+    }
+  }
+
+  function errorMatchingString(e: Error, errorMatching: string) {
+    if (e.message === errorMatching) {
+      return {
+        value,
+        pass: true,
+        message: `to throw "${errorMatching}"`,
+      };
+    } else {
+      return {
+        value,
+        pass: false,
+        message: `to throw "${errorMatching}"`,
+      };
+    }
+  }
+
+  function errorMatchingClass(e: Error, errorMatching: Error) {
+    if (errorMatching instanceof e.constructor) {
+      if (e.message === errorMatching.message) {
+        return {
+          value,
+          pass: true,
+          message: `to throw ${errorMatching.constructor.name}("${errorMatching.message}")`,
+        };
+      } else {
+        return {
+          value,
+          pass: false,
+          message: `to throw ${errorMatching.constructor.name}("${errorMatching.message}")`,
+        };
+      }
+    } else {
+      return {
+        value,
+        pass: false,
+        message: `to throw ${errorMatching.constructor.name}("${errorMatching.message}")`,
+      };
+    }
+  }
+};
