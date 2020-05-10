987ddd891753ef28ee85fbe3853d66921c6d9d03
Kylee Tilley <kyleetilley@gmail.com>
2020-05-10 00:16:16 -0500

    Refactor

--- src/assertions.test.ts
+++ src/assertions.test.ts
@@ -29,100 +29,106 @@ describe("assertions", () => {
   });
 
   describe("toThrow", () => {
-    it("should return passing assertion if function throws", () => {
-      const expectedFn = () => {
-        throw new Error();
-      };
+    describe("when called with no arguments", () => {
+      it("should return passing assertion if function throws", () => {
+        const expectedFn = () => {
+          throw new Error();
+        };
 
-      expect(toThrow()(expectedFn)).toEqual({
-        value: expectedFn,
-        pass: true,
-        message: `to throw`,
+        expect(toThrow()(expectedFn)).toEqual({
+          value: expectedFn,
+          pass: true,
+          message: `to throw`,
+        });
+      });
+
+      it("should return failing assertion if function doesn't throws", () => {
+        const expectedFn = () => {};
+
+        expect(toThrow()(expectedFn)).toEqual({
+          value: expectedFn,
+          pass: false,
+          message: `to throw`,
+        });
       });
     });
 
-    it("should return failing assertion if function doesn't throws", () => {
-      const expectedFn = () => {};
+    describe("when called with an expected error message", () => {
+      it("should return passing assertion if matches error message", () => {
+        const expectedErrorMessage = "expectedErrorMessage";
+        const expectedFn = () => {
+          throw new Error(expectedErrorMessage);
+        };
 
-      expect(toThrow()(expectedFn)).toEqual({
-        value: expectedFn,
-        pass: false,
-        message: `to throw`,
+        expect(toThrow(expectedErrorMessage)(expectedFn)).toEqual({
+          value: expectedFn,
+          pass: true,
+          message: `to throw "${expectedErrorMessage}"`,
+        });
+      });
+
+      it("should return failing assertion if doesn't match error message", () => {
+        const expectedErrorMessage = "expectedErrorMessage";
+        const expectedFn = () => {
+          throw new Error();
+        };
+
+        expect(toThrow(expectedErrorMessage)(expectedFn)).toEqual({
+          value: expectedFn,
+          pass: false,
+          message: `to throw "${expectedErrorMessage}"`,
+        });
       });
     });
 
-    it("should return passing assertion string message matches error message", () => {
-      const expectedErrorMessage = "expectedErrorMessage";
-      const expectedFn = () => {
-        throw new Error(expectedErrorMessage);
-      };
+    describe("when called with an expected error class and message", () => {
+      it("should return passing assertion if matches error class and message", () => {
+        class CustomError extends Error {}
 
-      expect(toThrow(expectedErrorMessage)(expectedFn)).toEqual({
-        value: expectedFn,
-        pass: true,
-        message: `to throw "${expectedErrorMessage}"`,
+        const expectedErrorMessage = "expectedErrorMessage";
+        const expectedFn = () => {
+          throw new CustomError(expectedErrorMessage);
+        };
+
+        expect(
+          toThrow(new CustomError(expectedErrorMessage))(expectedFn)
+        ).toEqual({
+          value: expectedFn,
+          pass: true,
+          message: `to throw CustomError("${expectedErrorMessage}")`,
+        });
       });
-    });
 
-    it("should return failing assertion string message doesn't match error message", () => {
-      const expectedErrorMessage = "expectedErrorMessage";
-      const expectedFn = () => {
-        throw new Error();
-      };
+      it("should return failing assertion if doesn't match error class", () => {
+        class CustomError extends Error {}
 
-      expect(toThrow(expectedErrorMessage)(expectedFn)).toEqual({
-        value: expectedFn,
-        pass: false,
-        message: `to throw "${expectedErrorMessage}"`,
+        const expectedErrorMessage = "expectedErrorMessage";
+        const expectedFn = () => {
+          throw new CustomError(expectedErrorMessage);
+        };
+
+        expect(toThrow(new Error(expectedErrorMessage))(expectedFn)).toEqual({
+          value: expectedFn,
+          pass: false,
+          message: `to throw Error("${expectedErrorMessage}")`,
+        });
       });
-    });
 
-    it("should return passing assertion error instance matches class and error message", () => {
-      class CustomError extends Error {}
+      it("should return failing assertion if doesn't match error message", () => {
+        class CustomError extends Error {}
 
-      const expectedErrorMessage = "expectedErrorMessage";
-      const expectedFn = () => {
-        throw new CustomError(expectedErrorMessage);
-      };
+        const expectedErrorMessage = "expectedErrorMessage";
+        const expectedFn = () => {
+          throw new CustomError();
+        };
 
-      expect(
-        toThrow(new CustomError(expectedErrorMessage))(expectedFn)
-      ).toEqual({
-        value: expectedFn,
-        pass: true,
-        message: `to throw CustomError("${expectedErrorMessage}")`,
-      });
-    });
-
-    it("should return failing assertion error instance doesn't match class", () => {
-      class CustomError extends Error {}
-
-      const expectedErrorMessage = "expectedErrorMessage";
-      const expectedFn = () => {
-        throw new CustomError(expectedErrorMessage);
-      };
-
-      expect(toThrow(new Error(expectedErrorMessage))(expectedFn)).toEqual({
-        value: expectedFn,
-        pass: false,
-        message: `to throw Error("${expectedErrorMessage}")`,
-      });
-    });
-
-    it("should return failing assertion error instance doesn't match error message", () => {
-      class CustomError extends Error {}
-
-      const expectedErrorMessage = "expectedErrorMessage";
-      const expectedFn = () => {
-        throw new CustomError();
-      };
-
-      expect(
-        toThrow(new CustomError(expectedErrorMessage))(expectedFn)
-      ).toEqual({
-        value: expectedFn,
-        pass: false,
-        message: `to throw CustomError("${expectedErrorMessage}")`,
+        expect(
+          toThrow(new CustomError(expectedErrorMessage))(expectedFn)
+        ).toEqual({
+          value: expectedFn,
+          pass: false,
+          message: `to throw CustomError("${expectedErrorMessage}")`,
+        });
       });
     });
   });
