750b3096855cf2f66e01e4e29a4379de4480d785
Kylee Tilley <kyleetilley@gmail.com>
2020-05-10 11:43:12 -0500

    Add comments to expect

--- src/expect.ts
+++ src/expect.ts
@@ -1,19 +1,34 @@
 import { AssertionFn, Assertion } from "./assertions";
 import { AssertionError } from "assert";
 
+/**
+ * Configure an instance of expect to use custom assertion error
+ * @param mapToError Function to make an assertion to an error to be thrown
+ * @returns Configured expect instance
+ */
 export function makeExpect<E extends Error>(
   mapToError: (assertion: Assertion<any>) => E
 ) {
-  return function expect<T>(value: T, assertionFn: AssertionFn<T>): void {
+  /**
+   * Test value against assertion function
+   * @param value Value under test
+   * @param assertionFn Function to assert on value
+   */
+  function expect<T>(value: T, assertionFn: AssertionFn<T>): void {
     const assertion = assertionFn.call(null, value);
 
     if (!assertion.pass) {
       const error = mapToError.call(null, assertion);
       throw error;
     }
-  };
+  }
+
+  return expect;
 }
 
+/**
+ * Default expect
+ */
 export const expect = makeExpect(
   ({ value, message }) => new AssertionError({ actual: value, message })
 );
